2e1693f7346b67e25c69a8e5c3a7161431cb761d
katie Lackey <lackeykn@muohio.edu>
2012-08-29 15:26:21 -0400

    added the rectangle!!!
    changed the color of the rectangle and the side bar color as well

--- src/CatPicApp.cpp
+++ src/CatPicApp.cpp
@@ -16,6 +16,7 @@ class CatPicApp : public AppBasic {
 	void mouseDown( MouseEvent event );	
 	void update();
 	void draw();
+	void prepareSettings(Settings* settings);
 private:
 	//Brightness of the screen
 	//float brightness;
@@ -41,8 +42,23 @@ void CatPicApp::CreateRectangle(uint8_t* pixels, int x1, int y1, int x2, int y2)
 	if(starty >= kAppHeight) return;
 	if(endx >= kAppWidth) endx = kAppWidth-1;
 	if(endy >= kAppHeight) endy = kAppHeight-1;
+
+	//rndColor = Rand::randInt (0,255);		
+	for ( int y=50; y <= endy; y++){		
+		for ( int x = 0; x <= endx; x++) {		
+			pixels [3* (x+y*endx)]=40;			
+			pixels [3* (x+y*endx)+1]=80;			
+			pixels [3* (x+y*endx)+2]=30;					
+		}		
+	}	
 }
 
+	
+
+void CatPicApp::prepareSettings(Settings* settings){
+	(*settings).setWindowSize(kAppWidth,kAppHeight);
+	(*settings).setResizable(false);
+}
 void CatPicApp::setup(){
 	mySurface_=new Surface(kTextureSize, kTextureSize, false);
 	
@@ -54,8 +70,9 @@ void CatPicApp::mouseDown( MouseEvent event )
 }
 
 void CatPicApp::update(){
-	//needed to draw the application on the screen
 	uint8_t* dataArray = (*mySurface_).getData();
+	//needed to draw the application on the screen
+	CreateRectangle(dataArray, 500,300,500,300);
 	
 	
 	//brightness = brightness + .01;
@@ -65,8 +82,9 @@ void CatPicApp::update(){
 
 void CatPicApp::draw(){
 
-	//Draw our surface to the screen, using graphics library
-	gl::clear(Color(.01,2,.02));
+	//Clear the surface with a green color
+	gl::clear(Color(1,3,5));
+	//Draw the surface
 	gl::draw(*mySurface_);
 	// clear out the window with black
 	//gl::clear( Color( .01, 2, .01 ) ); 
