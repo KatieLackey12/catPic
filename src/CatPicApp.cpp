2dc958ede8c2be6e232263a5fc7518b22c6b6712
katie Lackey <lackeykn@muohio.edu>
2012-08-29 15:54:47 -0400

    trying to add the line but having problems
    I think all the coding is implimented but the line does not appear on
    the screen... : (

--- src/CatPicApp.cpp
+++ src/CatPicApp.cpp
@@ -21,15 +21,16 @@ private:
 	//Brightness of the screen
 	//float brightness;
 	Surface* mySurface_; 
-
 	//width and height of the screen
 	static const int kAppWidth=800;
 	static const int kAppHeight=600;
 	static const int kTextureSize=1024;
-
+	
 void CreateRectangle(uint8_t* pixels, int x1, int y1, int x2, int y2);
+void drawLine(uint8_t*pixels, int line1_width, int line2_height);
+
 };
-void CatPicApp::CreateRectangle(uint8_t* pixels, int x1, int y1, int x2, int y2){
+void CatPicApp::CreateRectangle(uint8_t* dataArray, int x1, int y1, int x2, int y2){
 	int startx = (x1 < x2) ? x1 : x2;
 	int endx = (x1 < x2) ? x2 : x1;
 	int starty = (y1 < y2) ? y1 : y2;
@@ -43,14 +44,22 @@ void CatPicApp::CreateRectangle(uint8_t* pixels, int x1, int y1, int x2, int y2)
 	if(endx >= kAppWidth) endx = kAppWidth-1;
 	if(endy >= kAppHeight) endy = kAppHeight-1;
 
-	//rndColor = Rand::randInt (0,255);		
-	for ( int y=50; y <= endy; y++){		
-		for ( int x = 0; x <= endx; x++) {		
-			pixels [3* (x+y*endx)]=40;			
-			pixels [3* (x+y*endx)+1]=80;			
-			pixels [3* (x+y*endx)+2]=30;					
+	//making the rectangle
+	for ( int y=10; y <= endy; y++){		
+		for ( int x = 20; x <= endx; x++) {		
+			dataArray [3* (x+y*endx)]=40;			
+			dataArray [3* (x+y*endx)+1]=80;			
+			dataArray [3* (x+y*endx)+2]=30;					
 		}		
-	}	
+	}
+}
+void CatPicApp::drawLine(uint8_t*dataArray, int line1_width, int line2_height){
+	
+	for(int i=0; i<=line1_width;i++){
+		dataArray[3*i]=20;
+		dataArray[3*i+1]=40;
+		dataArray[3*i+2]=60;
+}
 }
 
 	
@@ -73,6 +82,7 @@ void CatPicApp::update(){
 	uint8_t* dataArray = (*mySurface_).getData();
 	//needed to draw the application on the screen
 	CreateRectangle(dataArray, 500,300,500,300);
+	drawLine(dataArray,40,30);
 	
 	
 	//brightness = brightness + .01;
@@ -83,7 +93,7 @@ void CatPicApp::update(){
 void CatPicApp::draw(){
 
 	//Clear the surface with a green color
-	gl::clear(Color(1,3,5));
+	gl::clear(Color(0,3,5));
 	//Draw the surface
 	gl::draw(*mySurface_);
 	// clear out the window with black
