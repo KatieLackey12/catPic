8e38768cdd579dd6f2b714ffe1b264d7fd81e1c9
katie Lackey <lackeykn@muohio.edu>
2012-09-04 23:12:33 -0400

    complete!
    interaction works but the new  triangle appears behind everything.

--- src/CatPicApp.cpp
+++ src/CatPicApp.cpp
@@ -9,6 +9,7 @@ CSE 274
 #include "cinder/ImageIo.h"
 #include "boost/date_time/posix_time/posix_time.hpp"
 #include "Resources.h"
+#include "cinder/Rand.h"
 
 
 using namespace ci;
@@ -23,12 +24,14 @@ class CatPicApp : public AppBasic {
 	void draw();
 	void prepareSettings(Settings* settings);
 private:
-	//Brightness of the screen
-	//float brightness;
 	//surface area that will have the shapes on it
 	Surface* mySurface_; 
 	//an int used to make objects move
 	int draw_offset;
+	//int variables for the mouse interaction method
+       int tr_randSize;
+       int tr_x;
+       int tr_y;
 
 	//the variables for the drawCircle method
 	struct circleData{
@@ -36,15 +39,15 @@ private:
 		int y;
 		int r;
 	};
-	//uint8_t* my_blur_pattern_;
 	//width and height of the screen
 	static const int kAppWidth=800;
 	static const int kAppHeight=600;
 	static const int kSurfaceSize=1024;
 //Rectangle method that draws a rectangle onto the screen.	
 void CreateRectangle(uint8_t* pixels, int x1, int y1, int x2, int y2);
+//Method draws a line on the screen,
 void drawLine(uint8_t*pixels, int length, int ptOneh, int ptTwo);
-//Method draws a circle onto the screen.
+//There thee circle methods draw circles onto the screen in the shape on mickey house.
 void drawCircles(uint8_t* dataArray, int x, int y, int r);
 void drawCircle2(uint8_t* dataArray, int x, int y, int r);
 void drawCircle3(uint8_t* dataArray, int x, int y, int r);
@@ -52,14 +55,16 @@ void drawCircle3(uint8_t* dataArray, int x, int y, int r);
 void makeLine(uint8_t* dataArray, int x0, int y0, int x1, int y1);
 //Method blurs the edges of the shapes
 void selectiveBlur(uint8_t* blur_pattern);
-//void drawAccident(uint8_t* pixels, int center_x, int center_y, int r);
 //Method clears the screen each time it is rain by painting it white first. Then the shapes are painted ontop if it.
 void clear(uint8_t* dataArray);
 //Method draws a rectangle onto the screen
 void drawTriangle(uint8_t* dataArray, int sideLength, int pointX,int pointY);
 
 };
-
+/**This menthod clears the screen each time it
+is ran so that the screen goes white. The objects 
+are then painted over the white screen.
+*/
 void CatPicApp::clear(uint8_t* dataArray){
 	//making the rectangle
 	for ( int y=10; y <= kAppHeight-1; y++){		
@@ -73,7 +78,12 @@ void CatPicApp::clear(uint8_t* dataArray){
 }
 /** This method creates the rectangle that apprears on the screen when ran.
 It makes in 4 variables to create the rectangle
-filles requirement 1
+filles requirement A.1
+@param dataArray is pixels in the array
+@param x1 is the starting coordinate of x
+@param y1 is the starting coordinate of y
+@param x2 is the ending coordinate of x
+@param y2 is the ending coordinate of y
 */
 void CatPicApp::CreateRectangle(uint8_t* dataArray, int x1, int y1, int x2, int y2){
 	//inital variables that create the start and end points of the rectangle 
@@ -100,6 +110,13 @@ void CatPicApp::CreateRectangle(uint8_t* dataArray, int x1, int y1, int x2, int 
 		}		
 	}
 }
+/**This method draws a traingle on the screen
+fills reguirment B.1
+@param dataArray is the pixels
+@param sideLength is the length of each traingle side
+@param pointX is one of the traingle points
+@param pointY is antoehr traingle point
+*/
 void CatPicApp::drawTriangle(uint8_t* dataArray, int sideLength, int pointX,int pointY){
 if((pointX<=kAppWidth)&&((pointX + sideLength) <= kAppWidth)){
 
@@ -128,9 +145,12 @@ if((pointX<=kAppWidth)&&((pointX + sideLength) <= kAppWidth)){
 }
 }
 }
-
 /*This method draws a straight line when the program is ran. 
-fills requirement 1
+fills requirement A.3
+@param dataArray are the pixels
+@param length is the length of the line
+@param ptOne is a point on the line
+@param ptTwo is another point on the line
 */
 void CatPicApp::drawLine(uint8_t*dataArray, int length, int ptOne, int ptTwo){
 	//making a line
@@ -150,14 +170,14 @@ void CatPicApp::prepareSettings(Settings* settings){
 	(*settings).setResizable(false);
 }
 
-
-void CatPicApp::mouseDown( MouseEvent event )
-{
-}
 /**This method draws a circle on the screen when ran.
 Three variables are taken in during this method to create the circle.
 This circle is also used to create part of the mickey mouse outline.
-fills requirement 1 and also helps fill the picture requirement 
+fills requirement A.2 and also helps fill the picture requirement 
+@Param dataArray are the pixels
+@param center_x is the center x coordinate 
+@param center_y is the center y coordinate
+@param r is the radius of the cirlce 
 */
 void CatPicApp::drawCircles(uint8_t* dataArray, int center_x, int center_y, int r){
 	if(r<=0) return;	
@@ -167,16 +187,21 @@ void CatPicApp::drawCircles(uint8_t* dataArray, int center_x, int center_y, int 
 				continue;
 			int distanceFromCenter = (int)sqrt((double)((x-center_x)*(x-center_x) + (y-center_y)*(y-center_y)));
 			if(distanceFromCenter <=r){
-				int offset = 3*(x+y*kSurfaceSize);
-			dataArray[offset] = 50;			
-			dataArray[offset+1] = 30;			
-			dataArray[offset+2] = 102;					
+				int equation = 3*(x+y*kSurfaceSize);
+			dataArray[equation] = 50;			
+			dataArray[equation+1] = 30;			
+			dataArray[equation+2] = 102;					
 		}
 		}
 	}
 }
-//This circle is used to create part of the mickey mouse outline
-//helps fill the picture requirement  
+/*This circle is used to create part of the mickey mouse outline
+helps fill the picture requirement  
+@Param dataArray are the pixels
+@param center_x is the center x coordinate 
+@param center_y is the center y coordinate
+@param r is the radius of the cirlce 
+*/
 void CatPicApp:: drawCircle2(uint8_t* dataArray, int center_x, int center_y, int r){
 if(r<=0) return;	
 	for (int y=center_y-r; y<=center_y+r; y++){		
@@ -193,8 +218,13 @@ if(r<=0) return;
 		}
 	}
 }
-//This method is used to creat part of the mickey mouse outline.
-//Helps fill the picture requirement 
+/*This method is used to creat part of the mickey mouse outline.
+Helps fill the picture requirement 
+@Param dataArray are the pixels
+@param center_x is the center x coordinate 
+@param center_y is the center y coordinate
+@param r is the radius of the cirlce 
+*/
 void CatPicApp:: drawCircle3(uint8_t* dataArray, int center_x, int center_y, int r){
 if(r<=0) return;	
 	for (int y=center_y-r; y<=center_y+r; y++){		
@@ -214,7 +244,7 @@ if(r<=0) return;
 }
 /**This method is a blue method and it blues the edges of the shapes
 to make the look less crsip.
-fills requirement 2
+fills requirement B.1
 */
 void CatPicApp::selectiveBlur(uint8_t* blur_pattern){
 	for(int y=1; y<= kAppHeight-1; y++){
@@ -224,7 +254,6 @@ void CatPicApp::selectiveBlur(uint8_t* blur_pattern){
 			int totalB=0;
 			for(int innerY=y-1; innerY <= y+1; innerY++){
 				for(int innerX=x-1; innerX <= x+1; innerX++){
-				//calculate offset for innerX and innerY
 					totalR = totalR + blur_pattern[3*(innerY+innerX*kSurfaceSize)];
 					totalG = totalG + blur_pattern[3*(innerY+innerX*kSurfaceSize)+1];
 					totalB = totalB + blur_pattern[3*(innerY+innerX*kSurfaceSize)+2];
@@ -233,40 +262,53 @@ void CatPicApp::selectiveBlur(uint8_t* blur_pattern){
 			blur_pattern[3*(y+x*kSurfaceSize)]=totalR/9;
 			blur_pattern[3*(y+x*kSurfaceSize)+1]=totalG/9;
 			blur_pattern[3*(y+x*kSurfaceSize)+2]=totalB/9;
-		}//calculate the offset for y and x
+		}
 	}
 }
 
 /**This method makes a few line segmenst that go across the screen
 when ran. It is a bunch of dots that a alligned very close to each other to
 look like a line.
-fills requirement 1
+fills requirement A.3
+@param dataArray are the pixels
+@param ptX0 is the staring x coordinate 
+@param ptY0 is the staring y coordinate
+@param ptX1 is the ending x coordinate
+@param ptY1 is the ending y coordinate
 */
-void CatPicApp:: makeLine(uint8_t* dataArray, int pt1x, int pt1y, int pt2x, int pt2y){
+void CatPicApp:: makeLine(uint8_t* dataArray, int ptX0, int ptY0, int ptX1, int ptY1){
 	int x, offset;
-	float disy, disx, y, midPt;
-	disy = pt2y-pt1y;
-	disx = pt2x-pt1x;
-	midPt = disy/disx;//find the midpoint
-	y = pt1y;
+	float dy, dx, y, midPt;
+	dy = ptY1 - ptY0;	
+	dx = ptX1 - ptX0;	
+	midPt = dy / dx;	
+	y = ptY0;
 
-	for(x = pt1x; x <= pt2x; x++){
+	for(x = ptX0; x <= ptX1; x++){
 		offset = 3 *(x + (y*kSurfaceSize));
-
 		dataArray[offset]=225;
 		dataArray[offset+1]=0;
 		dataArray[offset+2]=0;
 		y = y+midPt; 
 	}
 }
+/**This method allows for interaction with the user while the 
+project is being ran.
+*/
+void CatPicApp::mouseDown( MouseEvent event ){	
+tr_randSize = Rand::randInt(0, 300);	
+tr_x = event.getX();	
+tr_y = event.getY();
 
+}
 /**The update() method updates the program before it is ran each
 time. Anything  that you want to appear when ran must be declared here.
 */
 void CatPicApp::update(){
-	
-	uint8_t* dataArray = (*mySurface_).getData();
 	//needed to draw the application on the screen
+	uint8_t* dataArray = (*mySurface_).getData();
+	//allows for interaction with the triangle while the program is running 
+	drawTriangle(dataArray,tr_x, tr_y, tr_randSize);
 
 	//Clear the screen
 	clear(dataArray);
@@ -280,20 +322,17 @@ void CatPicApp::update(){
        makeLine(dataArray, 0, 0,300, 500);
        selectiveBlur(dataArray);
 	   drawTriangle(dataArray, 60,120,140);
-	//brightness = brightness + .01;
-	//if(brightness>1)
-		//brightness=0;
-	//tint = tint +.2;
-	//if(frame_number_ == 0){
 		writeImage("lackeykn.png",*mySurface_);
 }
+//This method sets up the program
 void CatPicApp::setup(){
 	mySurface_ = new Surface(kSurfaceSize,kSurfaceSize,false);
-	//brightness = .1;
-	//tint =.1;
 	draw_offset=0;
-	
+	tr_randSize=140;
+       tr_x=60;
+       tr_y=120;
 }
+//Everything is drawn in this method. Without this method nothing would get drawn on the screen.
 void CatPicApp::draw(){
 
 	//Clear the surface with a green color
@@ -303,8 +342,8 @@ void CatPicApp::draw(){
 	// clear out the window with black
 	//gl::clear( Color( .01, 2, .01 ) );
 	//setting a timer so that the mickey face moves every second.
-	Timer t;
-       t.start();
+		Timer t;
+      t.start();
        while(t.getSeconds() < 1.00){
        }
        t.stop();
